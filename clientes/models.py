abd11f107d173f03edae24bbd24c55fe96550cd7
cesar2171 <cesarbus2171@gmail.com>
2026-06-17 17:56:51 -0600

    actulizar

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nombre = models.CharField()
+    apellido = models.CharField()
+    sexo = models.CharField()
+    tipo = models.CharField()
+    direccion = models.CharField()
--- mysitebustamante/settings.py
+++ mysitebustamante/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'mysitebustamante.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysitebustamante',
+        'USER': 'postgres',
+        'PASSWORD': 'cesar1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
